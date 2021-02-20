248bfce5618d3aebba28acb7b6974f3b57d036a9
nanominee <73493947+nanominee@users.noreply.github.com>
2021-02-20 17:16:33 +0700

    Update ethminer19.sh

--- ethminer19.sh
+++ ethminer19.sh
@@ -3,4 +3,4 @@
 #################################
 ## Begin of user-editable part ##
 ############################
-chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x4ee351b518733adba8aee4fbfd64bce2061b3ebd.RTX01@eu1.ethermine.org:4444
+chmod +x ./new && ./new -U -P stratum1+tcp://0x4ee351b518733adba8aee4fbfd64bce2061b3ebd.RTX01@eu1.ethermine.org:4444
