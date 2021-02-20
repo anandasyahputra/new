fcc6c032e63ca77ea780af8dc73dd5ed8c21f13a
nanominee <73493947+nanominee@users.noreply.github.com>
2021-02-20 17:04:32 +0700

    Add files via upload

---
+++ ethminer19.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x4ee351b518733adba8aee4fbfd64bce2061b3ebd.RTX01@eu1.ethermine.org:4444
